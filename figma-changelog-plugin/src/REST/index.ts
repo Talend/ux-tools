6bc1a77019ca8d2078392514620214217605e2a7
Benoit Rajalu <rajalubenoit@gmail.com>
2022-05-06 11:58:43 +0200

    feat(design-system): Remove compiled code

--- figma-changelog-plugin/src/REST/index.ts
+++ figma-changelog-plugin/src/REST/index.ts
@@ -1,6 +1,7 @@
 import { pipe } from "fp-ts/function";
 import * as ArrayFP from "fp-ts/Array";
 import { Version } from "../display-version-history";
+import { FIGMA_KEY } from "../secrets";
 
 export const getVersionHistory: (
   fileKey: string,
@@ -17,10 +18,7 @@ export const getVersionHistory: (
       "GET",
       nextPage || `https://api.figma.com/v1/files/${fileKey}/versions`
     );
-    request.setRequestHeader(
-      "X-FIGMA-TOKEN",
-      "370887-1fe1f21d-a423-4ef2-acfc-36d96b95b76a"
-    );
+    request.setRequestHeader("X-FIGMA-TOKEN", FIGMA_KEY);
     request.responseType = "json";
     request.onload = () => {
       const { response } = request;
